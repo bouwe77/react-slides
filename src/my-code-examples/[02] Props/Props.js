fc7a8af34603613a292b82ecdda4c82b994b1334
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-07-11 22:14:15 +0200

    .

---
+++ src/my-code-examples/[02] Props/Props.js
@@ -0,0 +1,11 @@
+export default () => null;
+
+// Other stuff to pass as props:
+// const name = "people";
+// const names = ["John", "Paul", "George", "Ringo"];
+// const capitalize = name => name.toUpperCase();
+// const reorder = name =>
+//   name
+//     .split("")
+//     .sort()
+//     .join("");
---
+++ src/my-code-examples/[03] State/Counter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const counter = 42;
+
+  return (
+    <>
+      <h3>{counter}</h3>
+    </>
+  );
+}
+
+export default App;
