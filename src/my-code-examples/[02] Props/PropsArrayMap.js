5a9785d6f5131d48d358980bc00484d58ea4ca70
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-08-11 22:55:57 +0200

    .

--- src/my-code-examples/[02] Props/PropsArrayMap.js
+++ src/my-code-examples/[02] Props/PropsArrayMap.js
@@ -17,3 +17,4 @@ export default App;
 // What we'll learn in this example:
 // * Pass an array of objects as props.
 // * Using array.map to create an array of React elements to render.
+// * Passing the key prop to a list of elements so React can identify each one of them.
