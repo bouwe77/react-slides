0f9e9a83694056622b7c24a51dc3b9e5266e5332
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-09-18 21:58:03 +0200

    .

--- src/my-code-examples/[04] State/Counter.js
+++ src/my-code-examples/[04] State/Counter.js
@@ -14,3 +14,5 @@ export default App;
 
 // What we'll learn in this example:
 // * How to use the useState hook to do make a component stateful.
+// * How to update state using a user event.
+// * That re-rendering takes place after the event handler function has finished.
