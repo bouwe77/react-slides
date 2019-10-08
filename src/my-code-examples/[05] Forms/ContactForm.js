701a59bd3d7183901bb6b68ba19fb63b5b5eb22e
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-10-08 22:47:07 +0200

    .

--- src/my-code-examples/[05] Forms/ContactForm.js
+++ src/my-code-examples/[05] Forms/ContactForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./ContactForm.module.css";
+//import validator from "./validation";
+//import { VALID, INVALID, UNDETERMINED } from "./validationStatuses";
 
 function App() {
   function fakeSendingToServer(name, email, message) {
@@ -34,3 +36,60 @@ function ContactForm({ sendToServer }) {
 }
 
 export default App;
+
+// =========== UNCONTROLLED ======================
+
+//let nameInput = createRef();
+
+//const [nameValid, setNameValid] = useState(UNDETERMINED);
+
+// var name = nameInput.current.value;
+// const newNameValid = validator.validateName(name) ? VALID : INVALID;
+// setNameValid(newNameValid);
+
+// if (newNameValid === VALID && newEmailValid === VALID && newMessageValid === VALID) {
+//   sendToServer(name, email, message);
+//   nameInput.current.value = "";
+//   emailInput.current.value = "";
+//   messageInput.current.value = "";
+// }
+
+// =========== CONTROLLED ======================
+
+// const submitAllowed = nameValid === VALID && emailValid === VALID && messageValid === VALID;
+// console.log(nameValid, emailValid, messageValid, submitAllowed);
+
+// function handleChange(event) {
+//   switch (event.target.name) {
+//     case "name":
+//       setNameValid(UNDETERMINED);
+//       setName(event.target.value);
+//       break;
+//     case "email":
+//       setEmailValid(UNDETERMINED);
+//       setEmail(event.target.value);
+//       break;
+//     case "message":
+//       setMessageValid(UNDETERMINED);
+//       setMessage(event.target.value);
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
+// function handleBlur(event) {
+//   switch (event.target.name) {
+//     case "name":
+//       setNameValid(validator.validateName(name) ? VALID : INVALID);
+//       break;
+//     case "email":
+//       setEmailValid(validator.validateEmail(email) ? VALID : INVALID);
+//       break;
+//     case "message":
+//       setMessageValid(validator.validateMessage(message) ? VALID : INVALID);
+//       break;
+//     default:
+//       break;
+//   }
+// }
--- src/my-slides/FormsSummary.js
+++ src/my-slides/FormsSummary.js
@@ -5,17 +5,17 @@ export default () => {
   const markdown = `
 # FORMS
 
-Uncontrolled: 
-- maintain their own state
-- access values via refs
-- values available on submit
-- for simple forms
+| **Uncontrolled**        |  | **Controlled**           | 
+| ------------- | --- | ------------- | 
+| maintain their own state      | <-> | you control state | 
+| access values via refs      | <-> | access values via state      | 
+| values available on submit | <-> | values always available      | 
+| for simple forms | <-> | for interactive, user-friendly forms |
 
-Controlled:
-- you control state
-- access values via state
-- values always available
-- for interactive, user-friendly forms
-    `;
+
+To choose, answer the following question: 
+
+WHEN do I need the entered values?
+`;
   return <MarkdownSlide markdown={markdown} />;
 };
